c87efd439e2733481f38badca121ee44ff20efd3
Comet Warrior <132081997+cometwarrior0@users.noreply.github.com>
2025-06-02 01:26:36 +0300

    frfr

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-import { handlePointerEvents } from './main-pointer-handler.mjs';
 import { initLayerHandler } from './layer-handler.mjs';
 
 document.getElementById('create-button').addEventListener('click', () => {
@@ -9,34 +8,12 @@ document.getElementById('create-button').addEventListener('click', () => {
         return;
     }
 
-    // placeCanvas(x, y);
-
     initLayerHandler(x,y);
 
     document.getElementById('fground').style.visibility = 'visible';
     document.getElementById('create-panel').remove();
     window.getSelection().removeAllRanges(); // Clear any existing selection
 });
-
-
-// function placeCanvas(x, y) {
-//     const canvasContainer = document.getElementById('placecanvas');
-//     canvasContainer.style.width = x + 'px';
-//     canvasContainer.style.height = y + 'px';
-
-//     const canvas = document.createElement('canvas');
-//     canvasContainer.appendChild(canvas);
-
-//     canvas.width = x;
-//     canvas.height = y;
-
-//     const offscreenCanvas = canvas.transferControlToOffscreen();
-
-//     const worker = new Worker('scripts/canvas-worker.js', { type: "module" });
-//     worker.postMessage({ type: 'init', canvas: offscreenCanvas }, [offscreenCanvas]);
-
-//     handlePointerEvents(worker, { x: x, y: y });
-// }
 
 function clamp(x, l = 1, u = 16384) {
     return Math.round(Math.min(Math.max(x, l), u));
